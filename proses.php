8f377fa88d71502d0213afa40fa545c2cc1fdccf
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2023-12-18 00:17:32 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -9,6 +9,7 @@ class AnggotaKelompok {
         }
     
         public function tambahAnggota($data) {
+                // Mengambil data dari formulir (parsing)
                 $nomor = $data['Nomor'];
                 $nama = $data['Nama_Lengkap'];
                 $nik = $data['Nomor_Induk'];
@@ -19,11 +20,13 @@ class AnggotaKelompok {
                 $jenis_tanaman = $data['Jenis_Tanaman'];
                 $browser_info = $_SERVER['HTTP_USER_AGENT'];
                 $ip_address = $_SERVER['REMOTE_ADDR'];
-        
+                
+                 // Query SQL untuk menambahkan anggota ke database
                 $query = "INSERT INTO anggota_kelompok (no, nama, nik, jenis_kelamin, umur, luas_lahan, tanggal_masuk, jenis_tanaman, browser_info, ip_address) VALUES ('$nomor', '$nama', '$nik', '$jenis_kelamin', '$umur', '$luas_lahan', '$tanggal_masuk', '$jenis_tanaman', '$browser_info', '$ip_address')";
-        
+
+                // Eksekusi query dan cek apakah berhasil atau tidak
                 if (mysqli_query($this->konfigurasi, $query)) {
-                        echo "";
+                        echo ""; // Tidak perlu pesan sukses, karena diarahkan (redirect) ke halaman Beranda
                 } else {
                         echo "Pendaftaran terjadi error ! : " . $query . "<br>" . mysqli_error($this->konfigurasi);
                 }
@@ -33,10 +36,11 @@ class AnggotaKelompok {
         }
     
         public function hapusAnggota($nomor) {
+            // Query SQL untuk menghapus anggota berdasarkan Nomor
             $delete_query = "DELETE FROM anggota_kelompok WHERE no = '$nomor'";
-    
+            // Eksekusi query hapus dan cek apakah berhasil atau tidak
             if (mysqli_query($this->konfigurasi, $delete_query)) {
-                echo "";
+                echo ""; // Tidak perlu pesan sukses, karena diarahkan (redirect) ke halaman Beranda
             } else {
                 echo "Pendaftaran terjadi error ! : " . $delete_query . "<br>" . mysqli_error($this->konfigurasi);
             }
